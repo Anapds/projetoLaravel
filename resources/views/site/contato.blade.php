504cc5d0d20ea8a860f207098bef097fee4beba9
ana <paulati1102@gmail.com>
2025-03-11 16:23:14 -0300

    primeiro commit

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,12 @@
+<h1>OIE</h1>
+<ul>
+        <li>
+            <a href="{{route('site.index')}}">Principal</a>
+        </li>
+        <li>
+            <a href="{{route('site.sobreNos')}}">Sobre Nós</a>
+        </li>
+        <li>
+            <a href="{{route('site.contato')}}">Contato</a>
+        </li>
+    </ul>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', 'PrincipalController@Principal')->name('site.index');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobreNos');
+
+Route::get('/contato', 'contatoController@contato')->name('site.contato');
+
+Route::get('/login', function() {return 'Login';})->name('site.login');
+
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function() {return 'clientes';})->name('app.clientes');
+
+    Route::get('/fornecedores', function() {return 'fornecedores';})->name('app.fornecedores');
+    
+    Route::get('/produtos', function() {return 'produtos';})->name('app.produtos');
+});
+
+
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'"> clique aqui </a> para ir para a página inicial';
+});
